cef128db1a31e6e1b0716da5a978de8daae6ad32
Tayfun Biyikoglu <tayfunbiyikoglu@gmail.com>
2011-03-20 14:10:40 +0100

    User Authentication and show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,44 @@
 # == Schema Information
-# Schema version: 20110315212714
+# Schema version: 20110316222024
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name,:email
+  attr_accessor :password
+  attr_accessible :name,:email, :password,:password_confirmation,:salt
   
-  validates_presence_of :name,:email
-  validates_length_of :name, :minimum => 2,:maximum => 6
-end
+  validates :password, :presence => true, :confirmation => true, :length => { :within => 6..40 }, :on => :create
+  validates :name, :presence => true,  :length => { :maximum => 50  }
+    
+  before_save :encrypt_password
+  
+  def self.authenticate(email,password)
+    user = find_by_email(email)
+    if user && user.encrypted_password = BCrypt::Engine.hash_secret(password, user.salt)
+      user
+    else
+      nil
+    end
+    
+  end
+  
+  private
+  
+  def encrypt_password
+    if password.present?  
+       self.salt = BCrypt::Engine.generate_salt  
+       self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)  
+     end  
+  end
+  
+  
+ end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @attr = {:name => "Example User" , :email =>"example@example.com" }
+    @attr = {
+      :name => "Example User" ,
+      :email =>"example@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar",
+      :salt => "deneme"
+    }
   end
   
   it "should create a new user with give attributes" do
@@ -12,5 +18,27 @@ describe User do
   it "should require name" do
     @attr[:name] = ""
       User.new(@attr).should_not be_valid
+  end
+  
+  describe "Password_validations" do
+    
+    it "should require a password" do
+      User.new(@attr.merge(:password => "")).should_not be_valid
+    end
+    
+    it "should_require_a_matching_password_confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      short = "a"*5
+      User.new(@attr.merge(:password => short, :password_confirmation => short)).should_not be_valid
+    end
+    
+    it "should reject long passwords" do
+          long = "a"*41
+          User.new(@attr.merge(:password => long, :password_confirmation => long)).should_not be_valid
+     end
+    
   end
 end
